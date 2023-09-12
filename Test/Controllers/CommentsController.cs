6f0686df263a4768b9dc986a4770ec82e1202b0c
Arvid Ljungberg <arvid.lju@gmail.com>
2023-09-12 19:38:02 +0200

    Created better file structure

---
+++ Test/Controllers/CommentsController.cs
@@ -0,0 +1,72 @@
+﻿using GroupProj1Weddit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Test.Controllers;
+using Test.Data;
+using Test.Models;
+
+namespace GroupProj1Weddit.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ILogger<CommentsController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public CommentsController(ILogger<CommentsController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index(int id)
+        {
+            var post = _context.Posts.Include(p => p.Comments).FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        public IActionResult CreateComment(int id)
+        {
+            // Retrieve the post based on the id parameter
+            var post = _context.Posts.FirstOrDefault(p => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            // Create a new Comment and associate it with the post
+            var comment = new Comment();
+            comment.PostId = post.Id;
+
+            return View(comment);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(Comment comment)
+        {
+            Console.WriteLine();
+            if (ModelState.IsValid)
+            {
+                // Save the comment to the database
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+
+                // Redirect back to the "Comments" view for the same post
+                return RedirectToAction("Index", new { id = comment.PostId });
+            }
+
+            // If validation fails, return the view with validation errors
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Test/Controllers/PostsController.cs
@@ -0,0 +1,74 @@
+﻿using GroupProj1Weddit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Test.Controllers;
+using Test.Data;
+using Test.Models;
+
+namespace GroupProj1Weddit.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ILogger<PostsController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public PostsController(ILogger<PostsController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index(int id)
+        {
+            var topic = _context.Topics.Include(t => t.Posts).FirstOrDefault(t => t.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return View(topic);
+        }
+
+        public IActionResult CreatePost(int id)
+        {
+            // Retrieve the topic based on the id parameter
+            var topic = _context.Topics.FirstOrDefault(t => t.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            // Create a new Post and associate it with the topic
+            var post = new Post();
+            post.TopicId = topic.Id;
+            // Assign the user ID to the post
+            //post.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(Post post)
+        {
+            Console.WriteLine();
+            if (ModelState.IsValid)
+            {
+                // Save the post to the database
+                _context.Posts.Add(post);
+                _context.SaveChanges();
+
+                // Redirect back to the "Posts" view for the same topic
+                return RedirectToAction("Index", new { id = post.TopicId });
+            }
+
+            // If validation fails, return the view with validation errors
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
